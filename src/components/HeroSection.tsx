ca7db4bbab3c4f7081b98f30ede116841d46f6f5
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-14 23:00:13 +0530

    Mission: Hero Section completed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ function HeroSection() {
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
         <Spotlight
-          className="-top-40 left-0 md:left-60 md:-top-20"
+          className="-top-40 left-0 md:left-72 md:-top-20"
           fill="white"
         />
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
@@ -19,7 +19,14 @@ function HeroSection() {
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
-          <Link href={"/courses"}>Explore courses</Link>
+          <Link href={"/courses"}>
+            <Button
+            borderRadius="1.75rem"
+            className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore Courses
+            </Button>
+          </Link>
         </div>
       </div>
     </div>

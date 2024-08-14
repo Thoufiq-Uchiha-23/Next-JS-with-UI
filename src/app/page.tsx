4d2d87785b294501d6995e69ace262c0cc2441a4
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-14 22:59:37 +0530

    added Hero Section, Featured Courses, Why Choose Us sections to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased text-white bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Power Mode On</h1>
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
   );
 }
